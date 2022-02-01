11617592036c876016cbfb78b4698124aefec8c6
Thomas <thomasdulompont.1@gmail.com>
2022-02-01 23:28:10 +0100

    Modifications

--- js/animations.js
+++ js/animations.js
@@ -1,25 +1,17 @@
 const body = document.querySelector("body");
-
-// BODY ON LOAD
-
-// window.addEventListener("load", function() {
-
-// })
+const nav = document.querySelector("nav");
 
 // NAVBAR
 
-document.querySelector("nav").addEventListener("click", displayNavbar);
+nav.addEventListener("click", displayNavbar);
 
 function displayNavbar() {
     const element = document.querySelector(".navbar-active");
     element.classList.toggle("navbar-active-display");
-}
+};
 
 
 // BUTTON NAVIGATION
-
-document.querySelector(".button-next").addEventListener("click", nextPage);
-document.querySelector(".button-before").addEventListener("click", beforePage);
 
 function animationNext() {
     const animation = `
@@ -30,30 +22,21 @@ function animationNext() {
     body.innerHTML = animation;
 }
 
-function nextPage() {
-    const text = document.getElementById("next").textContent;
-    animationNext();
-    setTimeout(function() {
-        window.location.href = `${text}.html`;
-    }, 400);
-}
-
-function beforePage() {
-    const text = document.getElementById("before").textContent;
-    animationNext();
-    setTimeout(function() {
-        window.location.href = `${text}.html`;
-    }, 400);
-}
-
 
 // FOOTER
-const footer = document.querySelector("footer");
-const footerText = `
-<h3>Contactons nous !</h3>
-<p>N'hésitez pas à me contacter pour parler de votre projet,</p>
-<p>je suis joignable par mail : <a href="mailto:thomasdulompont.1@gmail.com">thomasdulompont.1@gmail.com</a></p>
-<p>ou connectons nous sur Linkedin : <a href="https://www.linkedin.com/in/thomas-dulompont-087544228/">Thomas-Dlmp</a></p>
-`;
+autoFooter();
 
-footer.innerHTML += footerText;
+
+function autoFooter() {
+    const footer = document.querySelector("footer");
+    const footerText = `
+    <h3>Contactons nous !</h3>
+    <p>N'hésitez pas à me contacter pour parler de votre projet,</p>
+    <p>je suis joignable par mail : <a href="mailto:thomasdulompont.1@gmail.com">thomasdulompont.1@gmail.com</a></p>
+    <p>ou connectons nous sur Linkedin : <a href="https://www.linkedin.com/in/thomas-dulompont-087544228/">Thomas-Dlmp</a></p>
+    `;
+    if (footer === null) {
+    } else {
+        footer.innerHTML += footerText;
+    }
+} 
